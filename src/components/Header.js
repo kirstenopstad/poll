9b6441f4613dea99fdc3fab00668809dd7236b1b
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-23 09:31:18 -0800

    add brand + navbard styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import PropTypes from 'prop-types'
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
  
 
 const Header = ({ onSignOut }) => {
@@ -13,32 +14,26 @@ const Header = ({ onSignOut }) => {
 
   return(
     <React.Fragment>
-      <Nav className="justify-content-center">
-      <Nav.Item>
-        <Nav.Link as="li">Poll</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as="li">
-          <Link to="/build">Build</Link>
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as="li">
-          <Link to="/take">Take</Link>
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as="li">
-          <Link to="/login">Login / Register</Link>
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as="li" onClick={handleLogOut}>
-          Logout
-        </Nav.Link>
-      </Nav.Item>
-
+      <Navbar className="justify-content-center">
+      <Navbar.Brand>
+        Poll
+      </Navbar.Brand>
+      <Nav>
+          <Nav.Link as="li">
+            <Link to="/build">Build</Link>
+          </Nav.Link>
+          <Nav.Link as="li">
+            <Link to="/take">Take</Link>
+          </Nav.Link>
+          <Nav.Link as="li">
+            <Link to="/login">Login / Register</Link>
+          </Nav.Link>
+          <Nav.Link onClick={handleLogOut}>
+            Logout
+          </Nav.Link>
       </Nav>
+
+      </Navbar>
     </React.Fragment>
   );
 }
